884a5242f59013cc4f336acb81093041e92165d0
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-03-30 23:51:22 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const make_todo = async () => {
+    let list = [];
+    while (true) {
+        const todo = await inquirer.prompt([
+            { message: "what do you wants to add", type: "input", name: "todouser" },
+            { message: "Do you wants to add more", type: "list", name: "user", choices: ["yes", "no"] }
+        ]);
+        list.push(todo.todouser);
+        if (todo.user === "no") {
+            break;
+        }
+    }
+    list.forEach((e) => console.log(e));
+};
+make_todo();
