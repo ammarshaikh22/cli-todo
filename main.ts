8f7918452f28dd0afefd333ddb46f73d67540627
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-04-03 15:09:23 +0500

    e

--- main.ts
+++ main.ts
@@ -3,15 +3,15 @@ import inquirer from "inquirer";
 
 const make_todo = async() =>{
     let list = [];
-    
+    let turn = true
     while(true){
         const todo = await inquirer.prompt([
             { message: "what do you wants to add", type: "input", name: "todouser" },
-            { message: "Do you wants to add more", type: "list", name: "user", choices: ["yes", "no"] }
+            { message: "Do you wants to add more", type: "confirm", name: "user", default:true}
         ])
         
         list.push(todo.todouser)
-        if(todo.user === "no"){
+        if(todo.user !== turn){
             break;
         }
     }
