cddb88445ec4359aa3537c39fab23c05960dc9a1
ammarshaikh123 <144041358+ammarshaikh22@users.noreply.github.com>
2024-04-03 15:17:45 +0500

    e

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ import inquirer from "inquirer";
 const make_todo = async () => {
     let list = [];
     let turn = true;
-    while (true) {
+    while (turn) {
         const todo = await inquirer.prompt([
             { message: "what do you wants to add", type: "input", name: "todouser" },
             { message: "Do you wants to add more", type: "confirm", name: "user", default: true }
         ]);
-        list.push(todo.todouser);
+        if (todo.todouser.length === 0) {
+            console.log("please input some data to add in list");
+            turn = false;
+        }
+        else {
+            list.push(todo.todouser);
+        }
         if (todo.user !== turn) {
             break;
         }
